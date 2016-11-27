355d7ce4163a006161420f370e56ed595f7c1a96
Bobby <bwuphan@gmail.com>
2016-11-26 22:35:14 -0800

    first commit

--- spec/part1.js
+++ spec/part1.js
@@ -9,7 +9,9 @@
       });
 
       it('should return whatever value is passed into it', function() {
+
         var uniqueObject = {};
+        console.log('hello');
         expect(_.identity(1)).to.equal(1);
         expect(_.identity('string')).to.equal('string');
         expect(_.identity(false)).to.be.false;
